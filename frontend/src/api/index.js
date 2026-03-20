5f9955b53e4941f25cdc871f243eee9259f22bee
Guillena <guillena@hotmail.com>
2026-03-20 02:31:36 -0300

    Corregir API URL para usar el RENDER_EXTERNAL_URL de Render

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL 
-    ? `https://${import.meta.env.VITE_API_URL}/api` 
+    ? `${import.meta.env.VITE_API_URL}/api` 
     : 'http://localhost:5000/api'
 });
 
