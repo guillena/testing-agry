b9055ac59979811e28f4896b536a6935e45e5f4f
Guillena <guillena@hotmail.com>
2026-03-29 12:22:50 -0300

    Config port change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Esto abre la puerta (0.0.0.0) para peticiones externas
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173
+  },
+  preview: {
+    host: true, // Esto abre la puerta en Producción
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173
+  }
 })
